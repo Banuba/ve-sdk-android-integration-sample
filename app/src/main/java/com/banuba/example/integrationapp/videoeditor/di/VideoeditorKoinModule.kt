9aa670c0f26ca58d31c8ab524ed924ef46261d7d
Ilya Pechuro <ilyapechuro991@gmail.com>
2020-11-18 14:41:25 +0300

    Add export params provider

--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoeditorKoinModule.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoeditorKoinModule.kt
@@ -2,13 +2,17 @@ package com.banuba.example.integrationapp.videoeditor.di
 
 import android.app.Application
 import android.content.Context
+import android.net.Uri
 import android.view.View
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.banuba.android.sdk.camera.BanubaCameraSdkManager
 import com.banuba.android.sdk.camera.CameraSdkManager
-import com.banuba.example.integrationapp.videoeditor.impl.GlideImageLoader
+import com.banuba.example.integrationapp.R
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportFlowManager
+import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportParamsProvider
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportResultHandler
+import com.banuba.example.integrationapp.videoeditor.impl.GlideImageLoader
 import com.banuba.sdk.core.AREffectPlayerProvider
 import com.banuba.sdk.core.IUtilityManager
 import com.banuba.sdk.core.domain.ImageLoader
@@ -18,9 +22,10 @@ import com.banuba.sdk.effectplayer.adapter.BanubaClassFactory
 import com.banuba.sdk.ve.flow.ExportFlowManager
 import com.banuba.sdk.ve.flow.ExportResultHandler
 import com.banuba.sdk.ve.flow.FlowEditorModule
-import com.banuba.sdk.ve.flow.R
+import com.banuba.sdk.veui.data.ExportParamsProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.definition.BeanDefinition
+import org.koin.core.qualifier.named
 
 /**
  * All dependencies mentioned in this module will override default
@@ -60,11 +65,39 @@ class VideoeditorKoinModule : FlowEditorModule() {
         }
 
     override val exportFlowManager: BeanDefinition<ExportFlowManager> = single {
-        IntegrationAppExportFlowManager()
+        IntegrationAppExportFlowManager(
+            exportDataProvider = get(),
+            editorSessionHelper = get(),
+            exportDir = get(named("exportDir")),
+            mediaFileNameHelper = get()
+        )
     }
 
     override val exportResultHandler: BeanDefinition<ExportResultHandler> = single {
         IntegrationAppExportResultHandler()
+    }
+
+    /**
+     * Provides params for export
+     * */
+    override val exportParamsProvider: BeanDefinition<ExportParamsProvider> =
+        factory(override = true) {
+            IntegrationAppExportParamsProvider(
+                exportDir = get(named("exportDir")),
+                sizeProvider = get(),
+                watermarkBuilder = get()
+            )
+        }
+
+    /**
+     * Provides path for exported files
+     * */
+    override val exportDir: BeanDefinition<Uri> = single(named("exportDir"), override = true) {
+        get<Context>().getExternalFilesDir("")
+            ?.toUri()
+            ?.buildUpon()
+            ?.appendPath("export")
+            ?.build() ?: throw NullPointerException("exportDir should't be null!")
     }
 
     override val imageLoader: BeanDefinition<ImageLoader> =
--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/export/IntegrationAppExportResultHandler.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/export/IntegrationAppExportResultHandler.kt
@@ -1,10 +1,24 @@
 package com.banuba.example.integrationapp.videoeditor.export
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.banuba.sdk.ve.flow.ExportResultHandler
+import com.banuba.sdk.veui.ui.EXTRA_EXPORTED_SUCCESS
 import com.banuba.sdk.veui.ui.ExportResult
 
 class IntegrationAppExportResultHandler: ExportResultHandler {
 
-    override fun doAction(activity: AppCompatActivity, result: ExportResult.Success?) {}
+    override fun doAction(activity: AppCompatActivity, result: ExportResult.Success?) {
+        if (result == null) {
+            activity.finish()
+            return
+        }
+        val resultCode = Activity.RESULT_OK
+        val data = Intent().apply {
+            putExtra(EXTRA_EXPORTED_SUCCESS, result)
+        }
+        activity.setResult(resultCode, data)
+        activity.finish()
+    }
 }
