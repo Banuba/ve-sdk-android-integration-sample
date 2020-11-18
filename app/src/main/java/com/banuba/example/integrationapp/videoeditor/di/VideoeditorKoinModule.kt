44d02e26a5ce188ceecf94853f7fabe6426530bc
Ilya Pechuro <ilyapechuro991@gmail.com>
2020-11-18 12:00:19 +0300

    Add editor effects

--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoeditorKoinModule.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoeditorKoinModule.kt
@@ -2,23 +2,28 @@ package com.banuba.example.integrationapp.videoeditor.di
 
 import android.app.Application
 import android.content.Context
+import android.net.Uri
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.banuba.android.sdk.camera.BanubaCameraSdkManager
 import com.banuba.android.sdk.camera.CameraSdkManager
-import com.banuba.example.integrationapp.videoeditor.impl.GlideImageLoader
+import com.banuba.example.integrationapp.R
+import com.banuba.example.integrationapp.videoeditor.data.MaskEffects
+import com.banuba.example.integrationapp.videoeditor.data.TimeEffects
+import com.banuba.example.integrationapp.videoeditor.data.VisualEffects
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportFlowManager
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportResultHandler
+import com.banuba.example.integrationapp.videoeditor.impl.GlideImageLoader
 import com.banuba.sdk.core.AREffectPlayerProvider
 import com.banuba.sdk.core.IUtilityManager
 import com.banuba.sdk.core.domain.ImageLoader
 import com.banuba.sdk.core.effects.EffectsResourceManager
 import com.banuba.sdk.effectplayer.adapter.BanubaAREffectPlayerProvider
 import com.banuba.sdk.effectplayer.adapter.BanubaClassFactory
+import com.banuba.sdk.ve.effects.EditorEffects
 import com.banuba.sdk.ve.flow.ExportFlowManager
 import com.banuba.sdk.ve.flow.ExportResultHandler
 import com.banuba.sdk.ve.flow.FlowEditorModule
-import com.banuba.sdk.ve.flow.R
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.definition.BeanDefinition
 
@@ -76,4 +81,33 @@ class VideoeditorKoinModule : FlowEditorModule() {
                 else -> throw IllegalArgumentException("Illegal source for GlideImageLoader")
             }
         }
+
+    override val editorEffects: BeanDefinition<EditorEffects> = single(override = true) {
+
+        val effectsResourceManager = get<EffectsResourceManager>()
+        val effectsUri = Uri.parse(effectsResourceManager.storageEffectsDir.absolutePath)
+            .buildUpon()
+            .appendPath("effects")
+            .build()
+
+        val visualEffects = listOf(
+            VisualEffects.VHS,
+            VisualEffects.Rave
+        )
+        val timeEffects = listOf(
+            TimeEffects.SlowMo(),
+            TimeEffects.Rapid()
+        )
+        val maskEffects = listOf(
+            MaskEffects.HawaiiHairFlower(effectsUri),
+            MaskEffects.AsaiLines(effectsUri)
+        )
+
+        EditorEffects(
+            visual = visualEffects,
+            time = timeEffects,
+            masks = maskEffects,
+            equalizer = emptyList()
+        )
+    }
 }
