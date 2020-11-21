9f6f6ab87d6f0ebae20aea034c8b649f261a07ec
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2020-11-21 12:51:56 +0300

    Add export params provider (#21)
    * Add export params provider
    
    * Add guide for export flow
    
    * Fix descriptions
    
    Co-authored-by: Ilya Pechuro <ilyapechuro991@gmail.com>
    Co-authored-by: Gleb Prischepa <gleb.prischepa@expcapital.com>
    Co-authored-by: Gleb <gleb.prischepa@banuba.com>

--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoEditorKoinModule.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoEditorKoinModule.kt
@@ -3,12 +3,14 @@ package com.banuba.example.integrationapp.videoeditor.di
 import android.content.Context
 import android.net.Uri
 import android.view.View
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.banuba.example.integrationapp.R
 import com.banuba.example.integrationapp.videoeditor.data.MaskEffects
 import com.banuba.example.integrationapp.videoeditor.data.TimeEffects
 import com.banuba.example.integrationapp.videoeditor.data.VisualEffects
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportFlowManager
+import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportParamsProvider
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportResultHandler
 import com.banuba.example.integrationapp.videoeditor.impl.GlideImageLoader
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppWatermarkProvider
@@ -18,13 +20,15 @@ import com.banuba.sdk.core.domain.ImageLoader
 import com.banuba.sdk.core.effects.EffectsResourceManager
 import com.banuba.sdk.effectplayer.adapter.BanubaAREffectPlayerProvider
 import com.banuba.sdk.effectplayer.adapter.BanubaClassFactory
-import com.banuba.sdk.ve.effects.WatermarkProvider
 import com.banuba.sdk.ve.effects.EditorEffects
+import com.banuba.sdk.ve.effects.WatermarkProvider
 import com.banuba.sdk.ve.flow.ExportFlowManager
 import com.banuba.sdk.ve.flow.ExportResultHandler
 import com.banuba.sdk.ve.flow.FlowEditorModule
+import com.banuba.sdk.veui.data.ExportParamsProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.definition.BeanDefinition
+import org.koin.core.qualifier.named
 
 /**
  * All dependencies mentioned in this module will override default
@@ -50,11 +54,39 @@ class VideoEditorKoinModule : FlowEditorModule() {
     }
 
     override val exportFlowManager: BeanDefinition<ExportFlowManager> = single {
-        IntegrationAppExportFlowManager()
+        IntegrationAppExportFlowManager(
+            exportDataProvider = get(),
+            editorSessionHelper = get(),
+            exportDir = get(named("exportDir")),
+            mediaFileNameHelper = get()
+        )
     }
 
     override val exportResultHandler: BeanDefinition<ExportResultHandler> = single {
         IntegrationAppExportResultHandler()
+    }
+
+    /**
+     * Provides params for export
+     * */
+    override val exportParamsProvider: BeanDefinition<ExportParamsProvider> =
+        factory(override = true) {
+            IntegrationAppExportParamsProvider(
+                exportDir = get(named("exportDir")),
+                sizeProvider = get(),
+                watermarkBuilder = get()
+            )
+        }
+
+    /**
+     * Provides path for exported files
+     * */
+    override val exportDir: BeanDefinition<Uri> = single(named("exportDir"), override = true) {
+        get<Context>().getExternalFilesDir("")
+            ?.toUri()
+            ?.buildUpon()
+            ?.appendPath("export")
+            ?.build() ?: throw NullPointerException("exportDir should't be null!")
     }
 
     override val imageLoader: BeanDefinition<ImageLoader> =
