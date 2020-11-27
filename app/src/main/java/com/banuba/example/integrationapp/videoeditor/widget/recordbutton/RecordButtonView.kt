525139ce5b1748381203a676433a237904b3e0fc
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2020-11-27 17:59:41 +0300

    Add camera record button animation provider (#20)
    * Add camera record button animation provider
    
    * Add guide for record button
    
    * add record button description
    
    Co-authored-by: Ilya Pechuro <ilyapechuro991@gmail.com>
    Co-authored-by: alexander_bernat <alexander.bernat@banuba.com>

---
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/widget/recordbutton/RecordButtonView.kt
@@ -0,0 +1,195 @@
+package com.banuba.example.integrationapp.videoeditor.widget.recordbutton
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
+import androidx.core.animation.doOnEnd
+import com.banuba.example.integrationapp.R
+import com.banuba.sdk.core.ui.ext.dimenPx
+
+internal class RecordButtonView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttrs: Int = 0
+) : FrameLayout(context, attributeSet, defStyleAttrs) {
+
+    companion object {
+
+        private const val DEFAULT_DURATION_MS = 300L
+        private const val FINISH_RECORD_DELAY = 200L
+
+        private const val PHOTO_SHOOT_ANIMATION_SCALE_START = 1F
+        private const val PHOTO_SHOOT_ANIMATION_SCALE_END = 0.875F
+
+        private const val VIDEO_SHOOT_INNER_ANIMATION_SCALE_START = 1F
+        private const val VIDEO_SHOOT_INNER_ANIMATION_SCALE_END = 0F
+
+        private const val VIDEO_SHOOT_INNER_ANIMATION_REPEAT_COUNT = 0
+
+        private const val PHOTO_SHOOT_OUTER_ANIMATION_START = 1F
+        private const val PHOTO_SHOOT_OUTER_ANIMATION_END = 1.5F
+    }
+
+    private val outerPart = RecordButtonOuterView(context)
+
+    private val innerOvalPart = View(context).apply {
+        setBackgroundResource(R.drawable.ic_record_button_inner_oval)
+        elevation = context.dimenPx(R.dimen.record_button_elevation).toFloat()
+    }
+    private val innerSquarePart = View(context).apply {
+        val size = context.dimenPx(R.dimen.record_button_inner_part_size)
+        layoutParams = LayoutParams(size, size).apply {
+            gravity = Gravity.CENTER
+        }
+        setBackgroundResource(R.drawable.ic_record_button_inner_square)
+        elevation = context.dimenPx(R.dimen.record_button_elevation).toFloat()
+    }
+
+    private val photoShootAnimation = ObjectAnimator.ofPropertyValuesHolder(
+        innerOvalPart,
+        PropertyValuesHolder.ofFloat(
+            "scaleX",
+            PHOTO_SHOOT_ANIMATION_SCALE_START,
+            PHOTO_SHOOT_ANIMATION_SCALE_END
+        ),
+        PropertyValuesHolder.ofFloat(
+            "scaleY",
+            PHOTO_SHOOT_ANIMATION_SCALE_START,
+            PHOTO_SHOOT_ANIMATION_SCALE_END
+        )
+    ).apply {
+        duration = DEFAULT_DURATION_MS
+        repeatCount = 1
+        repeatMode = ValueAnimator.REVERSE
+    }
+
+    private val videoShootInnerAnimation = AnimatorSet().apply {
+        val ovalAnim = ObjectAnimator.ofPropertyValuesHolder(
+            innerOvalPart,
+            PropertyValuesHolder.ofFloat(
+                "scaleX",
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_START,
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_END
+            ),
+            PropertyValuesHolder.ofFloat(
+                "scaleY",
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_START,
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_END
+            )
+        ).apply {
+            duration = DEFAULT_DURATION_MS
+            repeatCount = VIDEO_SHOOT_INNER_ANIMATION_REPEAT_COUNT
+        }
+        val squareAnim = ObjectAnimator.ofPropertyValuesHolder(
+            innerSquarePart,
+            PropertyValuesHolder.ofFloat(
+                "scaleX",
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_END,
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_START
+            ),
+            PropertyValuesHolder.ofFloat(
+                "scaleY",
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_END,
+                VIDEO_SHOOT_INNER_ANIMATION_SCALE_START
+            )
+        ).apply {
+            duration = DEFAULT_DURATION_MS
+            repeatCount = VIDEO_SHOOT_INNER_ANIMATION_REPEAT_COUNT
+        }
+        playTogether(ovalAnim, squareAnim)
+    }
+    private val videoShootOuterAnimation = AnimatorSet().apply {
+        val circleAnim = ObjectAnimator.ofPropertyValuesHolder(
+            outerPart,
+            PropertyValuesHolder.ofFloat(
+                "scaleX",
+                PHOTO_SHOOT_OUTER_ANIMATION_START,
+                PHOTO_SHOOT_OUTER_ANIMATION_END
+            ),
+            PropertyValuesHolder.ofFloat(
+                "scaleY",
+                PHOTO_SHOOT_OUTER_ANIMATION_START,
+                PHOTO_SHOOT_OUTER_ANIMATION_END
+            )
+        ).apply {
+            duration = DEFAULT_DURATION_MS
+            repeatCount = 0
+        }
+        playTogether(circleAnim)
+    }
+
+    init {
+        val outerPartSize = context.dimenPx(R.dimen.record_button_progress_size)
+        addView(outerPart, LayoutParams(outerPartSize, outerPartSize).apply {
+            gravity = Gravity.CENTER
+        })
+        addView(innerSquarePart)
+        addView(innerOvalPart)
+    }
+
+    fun reset() {
+        photoShootAnimation.cancel()
+        videoShootInnerAnimation.cancel()
+    }
+
+    fun animateTakePhoto(onEndCallback: () -> Unit) {
+        with(photoShootAnimation) {
+            doOnEnd { onEndCallback() }
+            start()
+        }
+    }
+
+    fun animateStartVideoRecord(
+        availableDurationMs: Long,
+        maxDurationMs: Long,
+        onEndCallback: () -> Unit
+    ) {
+        outerPart.startAnimation(
+            availableDurationMs = availableDurationMs,
+            maxDurationMs = maxDurationMs
+        )
+        with(videoShootInnerAnimation) {
+            interpolator = LinearInterpolator()
+            removeAllListeners()
+            doOnEnd { onEndCallback() }
+            start()
+        }
+        with(videoShootOuterAnimation) {
+            interpolator = LinearInterpolator()
+            start()
+        }
+    }
+
+    fun animateStopVideoRecord(onEndCallback: () -> Unit) {
+        outerPart.stopAnimation()
+        with(videoShootInnerAnimation) {
+            interpolator = ReverseInterpolator
+            removeAllListeners()
+            doOnEnd {
+                postDelayed({
+                    onEndCallback()
+                }, FINISH_RECORD_DELAY)
+            }
+            start()
+        }
+        with(videoShootOuterAnimation) {
+            interpolator = ReverseInterpolator
+            start()
+        }
+    }
+
+    fun pauseAnimation() {
+        outerPart.pauseAnimation()
+    }
+
+    fun resumeAnimation() {
+        outerPart.resumeAnimation()
+    }
+}
