0473507c2a2819240c363ada368dca87d852b521
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2021-07-13 16:48:33 +0300

    Migrate to 1.0.17 (#110)
    Co-authored-by: Alexander <alexander.bernat@banuba.com>

--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoEditorKoinModule.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/VideoEditorKoinModule.kt
@@ -1,25 +1,22 @@
 package com.banuba.example.integrationapp.videoeditor.di
 
+import androidx.fragment.app.Fragment
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportParamsProvider
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppColorFilterOrderProvider
-import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppRecordingAnimationProvider
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppWatermarkProvider
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationTimerStateProvider
 import com.banuba.sdk.arcloud.data.source.ArEffectsRepositoryProvider
 import com.banuba.sdk.audiobrowser.domain.AudioBrowserMusicProvider
-import com.banuba.sdk.cameraui.data.CameraRecordingAnimationProvider
 import com.banuba.sdk.cameraui.data.CameraTimerActionProvider
 import com.banuba.sdk.cameraui.data.CameraTimerStateProvider
+import com.banuba.sdk.cameraui.domain.HandsFreeTimerActionProvider
 import com.banuba.sdk.core.data.ColorFilterOrderProvider
 import com.banuba.sdk.core.domain.TrackData
-import com.banuba.sdk.core.pip.IPictureInPictureProvider
 import com.banuba.sdk.core.ui.ContentFeatureProvider
 import com.banuba.sdk.ve.effects.WatermarkProvider
 import com.banuba.sdk.ve.flow.ExportFlowManager
 import com.banuba.sdk.ve.flow.FlowEditorModule
 import com.banuba.sdk.ve.flow.export.ForegroundExportFlowManager
-import com.banuba.sdk.ve.flow.provider.HandsFreeTimerActionProvider
-import com.banuba.sdk.ve.pip.ExoPlayerPictureInPictureProvider
 import com.banuba.sdk.veui.data.ExportParamsProvider
 import com.banuba.sdk.veui.domain.CoverProvider
 import org.koin.core.definition.BeanDefinition
@@ -40,7 +37,8 @@ class VideoEditorKoinModule : FlowEditorModule() {
             exportDir = get(named("exportDir")),
             mediaFileNameHelper = get(),
             shouldClearSessionOnFinish = true,
-            publishManager = get()
+            publishManager = get(),
+            draftManager = get()
         )
     }
 
@@ -60,14 +58,6 @@ class VideoEditorKoinModule : FlowEditorModule() {
         IntegrationAppWatermarkProvider()
     }
 
-    /**
-     * Provides camera record button animation
-     * */
-    override val cameraRecordingAnimationProvider: BeanDefinition<CameraRecordingAnimationProvider> =
-        factory(override = true) {
-            IntegrationAppRecordingAnimationProvider(context = get())
-        }
-
     override val cameraTimerStateProvider: BeanDefinition<CameraTimerStateProvider> =
         factory(override = true) {
             IntegrationTimerStateProvider()
@@ -81,17 +71,13 @@ class VideoEditorKoinModule : FlowEditorModule() {
             )
         }
 
-    override val musicTrackProvider: BeanDefinition<ContentFeatureProvider<TrackData>> =
+    override val musicTrackProvider: BeanDefinition<ContentFeatureProvider<TrackData, Fragment>> =
         single(named("musicTrackProvider"), override = true) {
             AudioBrowserMusicProvider()
         }
 
     override val coverProvider: BeanDefinition<CoverProvider> = single(override = true) {
         CoverProvider.EXTENDED
-    }
-
-    override val pipProvider: BeanDefinition<IPictureInPictureProvider> = single(override = true) {
-        ExoPlayerPictureInPictureProvider()
     }
 
     override val cameraTimerActionProvider: BeanDefinition<CameraTimerActionProvider> =
