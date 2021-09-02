de170f480196f730b70c5fed3d770d416243e959
Eugeny Mironenko <eugeny.mironenko@banuba.com>
2021-09-02 13:44:24 +0300

    migrate to Banuba VideoEditor SDK 1.0.18.1

--- app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
+++ app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
@@ -6,8 +6,10 @@ import com.banuba.example.integrationapp.videoeditor.di.VideoEditorKoinModule;
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule;
 import com.banuba.sdk.audiobrowser.di.AudioBrowserKoinModule;
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule;
+import com.banuba.sdk.export.di.VeExportKoinModule;
 import com.banuba.sdk.gallery.di.GalleryKoinModule;
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule;
+import com.banuba.sdk.ve.di.VeSdkKoinModule;
 
 import org.koin.core.context.GlobalContext;
 
@@ -21,7 +23,10 @@ public class IntegrationJavaApp extends Application {
         super.onCreate();
         startKoin(GlobalContext.INSTANCE, koinApplication -> {
             androidContext(koinApplication, this);
+            // pass the customized Koin module that implements required dependencies. Keep order of modules
             koinApplication.modules(
+                    new VeSdkKoinModule().getModule(),
+                    new VeExportKoinModule().getModule(),
                     new AudioBrowserKoinModule().getModule(), // use this module only if you bought it
                     new ArCloudKoinModule().getModule(),
                     new TokenStorageKoinModule().getModule(),
