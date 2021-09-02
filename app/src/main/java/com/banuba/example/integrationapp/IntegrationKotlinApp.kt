a0319e7eeb0d0ca8f7f9ba2e8aafced4b80275bb
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2021-09-02 17:03:29 +0300

    migrate to Banuba VideoEditor SDK 1.0.18.1 (#126)
    Co-authored-by: Eugeny Mironenko <eugeny.mironenko@banuba.com>

--- app/src/main/java/com/banuba/example/integrationapp/IntegrationKotlinApp.kt
+++ app/src/main/java/com/banuba/example/integrationapp/IntegrationKotlinApp.kt
@@ -5,8 +5,10 @@ import com.banuba.example.integrationapp.videoeditor.di.VideoEditorKoinModule
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule
 import com.banuba.sdk.audiobrowser.di.AudioBrowserKoinModule
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule
+import com.banuba.sdk.export.di.VeExportKoinModule
 import com.banuba.sdk.gallery.di.GalleryKoinModule
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule
+import com.banuba.sdk.ve.di.VeSdkKoinModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -17,8 +19,10 @@ class IntegrationKotlinApp : Application() {
         startKoin {
             androidContext(this@IntegrationKotlinApp)
 
-            // pass the customized Koin module that implements required dependencies.
+            // pass the customized Koin module that implements required dependencies. Keep order of modules
             modules(
+                VeSdkKoinModule().module,
+                VeExportKoinModule().module,
                 AudioBrowserKoinModule().module, // use this module only if you bought it
                 ArCloudKoinModule().module,
                 TokenStorageKoinModule().module,
