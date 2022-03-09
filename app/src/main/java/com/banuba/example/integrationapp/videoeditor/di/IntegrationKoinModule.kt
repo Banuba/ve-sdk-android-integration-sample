6e9f181605d8c9e31f4fc013ce4798a386afb388
Alexander <alexander.bernat@banuba.com>
2022-03-09 09:27:17 +0100

    migrate to 1.22.0

--- app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/IntegrationKoinModule.kt
+++ app/src/main/java/com/banuba/example/integrationapp/videoeditor/di/IntegrationKoinModule.kt
@@ -1,6 +1,7 @@
 package com.banuba.example.integrationapp.videoeditor.di
 
 import androidx.fragment.app.Fragment
+import com.banuba.android.sdk.ve.timeline.`object`.data.ObjectEditorConfig
 import com.banuba.example.integrationapp.videoeditor.export.IntegrationAppExportParamsProvider
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppColorFilterOrderProvider
 import com.banuba.example.integrationapp.videoeditor.impl.IntegrationAppMaskOrderProvider
@@ -11,18 +12,20 @@ import com.banuba.sdk.audiobrowser.domain.AudioBrowserMusicProvider
 import com.banuba.sdk.cameraui.data.CameraTimerActionProvider
 import com.banuba.sdk.cameraui.data.CameraTimerStateProvider
 import com.banuba.sdk.cameraui.domain.HandsFreeTimerActionProvider
+import com.banuba.sdk.core.AspectRatio
 import com.banuba.sdk.core.data.OrderProvider
 import com.banuba.sdk.core.data.TrackData
+import com.banuba.sdk.core.domain.AspectRatioProvider
 import com.banuba.sdk.core.domain.DraftConfig
 import com.banuba.sdk.core.ui.ContentFeatureProvider
 import com.banuba.sdk.export.data.ExportFlowManager
 import com.banuba.sdk.export.data.ExportParamsProvider
 import com.banuba.sdk.export.data.ForegroundExportFlowManager
 import com.banuba.sdk.ve.effects.WatermarkProvider
-import com.banuba.sdk.ve.flow.FlowEditorModule
+import com.banuba.sdk.veui.data.EditorConfig
 import com.banuba.sdk.veui.domain.CoverProvider
-import org.koin.core.definition.BeanDefinition
 import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
 /**
  * All dependencies mentioned in this module will override default
@@ -30,27 +33,27 @@ import org.koin.core.qualifier.named
  * Some dependencies has no default implementations. It means that
  * these classes fully depends on your requirements
  */
-class VideoEditorKoinModule : FlowEditorModule() {
+class IntegrationKoinModule {
 
-    val exportFlowManager: BeanDefinition<ExportFlowManager> = single(override = true) {
-        ForegroundExportFlowManager(
-            exportDataProvider = get(),
-            sessionParamsProvider = get(),
-            exportSessionHelper = get(),
-            exportDir = get(named("exportDir")),
-            shouldClearSessionOnFinish = true,
-            publishManager = get(),
-            errorParser = get(),
-            mediaFileNameHelper = get(),
-            exportBundleProvider = get()
-        )
-    }
+    val module = module {
+        single<ExportFlowManager>(override = true) {
+            ForegroundExportFlowManager(
+                exportDataProvider = get(),
+                sessionParamsProvider = get(),
+                exportSessionHelper = get(),
+                exportDir = get(named("exportDir")),
+                shouldClearSessionOnFinish = true,
+                publishManager = get(),
+                errorParser = get(),
+                mediaFileNameHelper = get(),
+                exportBundleProvider = get()
+            )
+        }
 
-    /**
-     * Provides params for export
-     * */
-    val exportParamsProvider: BeanDefinition<ExportParamsProvider> =
-        factory(override = true) {
+        /**
+         * Provides params for export
+         * */
+        factory<ExportParamsProvider>(override = true) {
             IntegrationAppExportParamsProvider(
                 exportDir = get(named("exportDir")),
                 sizeProvider = get(),
@@ -58,48 +61,65 @@ class VideoEditorKoinModule : FlowEditorModule() {
             )
         }
 
-    val watermarkProvider: BeanDefinition<WatermarkProvider> = factory(override = true) {
-        IntegrationAppWatermarkProvider()
-    }
+        factory<WatermarkProvider>(override = true) {
+            IntegrationAppWatermarkProvider()
+        }
 
-    override val cameraTimerStateProvider: BeanDefinition<CameraTimerStateProvider> =
-        factory(override = true) {
+        factory<CameraTimerStateProvider>(override = true) {
             IntegrationTimerStateProvider()
         }
 
-    val arEffectsRepositoryProvider: BeanDefinition<ArEffectsRepositoryProvider> =
-        single(override = true, createdAtStart = true) {
+        single<ArEffectsRepositoryProvider>(override = true, createdAtStart = true) {
             ArEffectsRepositoryProvider(
                 arEffectsRepository = get(named("backendArEffectsRepository")),
                 ioDispatcher = get(named("ioDispatcher"))
             )
         }
 
-    override val musicTrackProvider: BeanDefinition<ContentFeatureProvider<TrackData, Fragment>> =
-        single(named("musicTrackProvider"), override = true) {
+        single<ContentFeatureProvider<TrackData, Fragment>>(
+            named("musicTrackProvider"),
+            override = true
+        ) {
             AudioBrowserMusicProvider()
         }
 
-    override val coverProvider: BeanDefinition<CoverProvider> = single(override = true) {
-        CoverProvider.EXTENDED
-    }
+        single<CoverProvider>(override = true) {
+            CoverProvider.EXTENDED
+        }
 
-    override val cameraTimerActionProvider: BeanDefinition<CameraTimerActionProvider> =
-        single(override = true) {
+        single<CameraTimerActionProvider>(override = true) {
             HandsFreeTimerActionProvider()
         }
 
-    override val colorFilterOrderProvider: BeanDefinition<OrderProvider> =
-        single(named("colorFilterOrderProvider"), override = true) {
+        single<OrderProvider>(named("colorFilterOrderProvider"), override = true) {
             IntegrationAppColorFilterOrderProvider()
         }
 
-    override val maskOrderProvider: BeanDefinition<OrderProvider> =
-        single(named("maskOrderProvider"), override = true) {
+        single<OrderProvider>(named("maskOrderProvider"), override = true) {
             IntegrationAppMaskOrderProvider()
         }
 
-    override val draftConfig: BeanDefinition<DraftConfig> = factory(override = true) {
-        DraftConfig.ENABLED_ASK_TO_SAVE
+        factory<DraftConfig>(override = true) {
+            DraftConfig.ENABLED_ASK_TO_SAVE
+        }
+
+        single<AspectRatioProvider>(override = true) {
+            object : AspectRatioProvider {
+                override fun provide(): AspectRatio = AspectRatio(9.0 / 16)
+            }
+        }
+
+        single<EditorConfig>(override = true) {
+            EditorConfig(
+                minTotalVideoDurationMs = 1500
+            )
+        }
+
+        single<ObjectEditorConfig>(override = true) {
+            ObjectEditorConfig(
+                objectEffectDefaultDuration = 2000
+            )
+        }
+
     }
 }
