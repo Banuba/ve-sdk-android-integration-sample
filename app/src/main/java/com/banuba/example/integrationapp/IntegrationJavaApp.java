f37c59cd5bb0a9c02f274851cc092a6519825083
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2022-03-15 18:57:04 +0300

    migrate to 1.22.0 (#171)
    Co-authored-by: Alexander <alexander.bernat@banuba.com>

--- app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
+++ app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
@@ -1,8 +1,11 @@
 package com.banuba.example.integrationapp;
 
+import static org.koin.android.ext.koin.KoinExtKt.androidContext;
+import static org.koin.core.context.GlobalContextExtKt.startKoin;
+
 import android.app.Application;
 
-import com.banuba.example.integrationapp.videoeditor.di.VideoEditorKoinModule;
+import com.banuba.example.integrationapp.videoeditor.di.IntegrationKoinModule;
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule;
 import com.banuba.sdk.audiobrowser.di.AudioBrowserKoinModule;
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule;
@@ -11,11 +14,9 @@ import com.banuba.sdk.gallery.di.GalleryKoinModule;
 import com.banuba.sdk.playback.di.VePlaybackSdkKoinModule;
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule;
 import com.banuba.sdk.ve.di.VeSdkKoinModule;
+import com.banuba.sdk.ve.flow.di.VeFlowKoinModule;
 
 import org.koin.core.context.GlobalContext;
-
-import static org.koin.android.ext.koin.KoinExtKt.androidContext;
-import static org.koin.core.context.GlobalContextExtKt.startKoin;
 
 public class IntegrationJavaApp extends Application {
 
@@ -32,7 +33,8 @@ public class IntegrationJavaApp extends Application {
                     new AudioBrowserKoinModule().getModule(), // use this module only if you bought it
                     new ArCloudKoinModule().getModule(),
                     new TokenStorageKoinModule().getModule(),
-                    new VideoEditorKoinModule().getModule(),
+                    new VeFlowKoinModule().getModule(),
+                    new IntegrationKoinModule().getModule(),
                     new GalleryKoinModule().getModule(),
                     new BanubaEffectPlayerKoinModule().getModule()
             );
--- app/src/main/java/com/banuba/example/integrationapp/IntegrationKotlinApp.kt
+++ app/src/main/java/com/banuba/example/integrationapp/IntegrationKotlinApp.kt
@@ -1,7 +1,7 @@
 package com.banuba.example.integrationapp
 
 import android.app.Application
-import com.banuba.example.integrationapp.videoeditor.di.VideoEditorKoinModule
+import com.banuba.example.integrationapp.videoeditor.di.IntegrationKoinModule
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule
 import com.banuba.sdk.audiobrowser.di.AudioBrowserKoinModule
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule
@@ -10,6 +10,7 @@ import com.banuba.sdk.gallery.di.GalleryKoinModule
 import com.banuba.sdk.playback.di.VePlaybackSdkKoinModule
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule
 import com.banuba.sdk.ve.di.VeSdkKoinModule
+import com.banuba.sdk.ve.flow.di.VeFlowKoinModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -28,7 +29,8 @@ class IntegrationKotlinApp : Application() {
                 AudioBrowserKoinModule().module, // use this module only if you bought it
                 ArCloudKoinModule().module,
                 TokenStorageKoinModule().module,
-                VideoEditorKoinModule().module,
+                VeFlowKoinModule().module,
+                IntegrationKoinModule().module,
                 GalleryKoinModule().module,
                 BanubaEffectPlayerKoinModule().module
             )
