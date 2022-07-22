8b6948c1a9833363061e202ed617e1646031ef2b
Maksim Abramenka <maksim.abramenka@banuba.com>
2022-07-22 17:46:23 +0300

    Fix of start Koin for java implementation.

--- app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
+++ app/src/main/java/com/banuba/example/integrationapp/IntegrationJavaApp.java
@@ -1,7 +1,7 @@
 package com.banuba.example.integrationapp;
 
 import static org.koin.android.ext.koin.KoinExtKt.androidContext;
-import static org.koin.core.context.GlobalContextExtKt.startKoin;
+import static org.koin.core.context.DefaultContextExtKt.startKoin;
 
 import android.app.Application;
 
@@ -15,15 +15,14 @@ import com.banuba.sdk.playback.di.VePlaybackSdkKoinModule;
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule;
 import com.banuba.sdk.ve.di.VeSdkKoinModule;
 import com.banuba.sdk.ve.flow.di.VeFlowKoinModule;
-
-import org.koin.core.context.GlobalContext;
+import com.banuba.sdk.veui.di.VeUiSdkKoinModule;
 
 public class IntegrationJavaApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        startKoin(GlobalContext.INSTANCE, koinApplication -> {
+        startKoin(koinApplication -> {
             androidContext(koinApplication, this);
             // pass the customized Koin module that implements required dependencies. Keep order of modules
             koinApplication.modules(
@@ -33,11 +32,12 @@ public class IntegrationJavaApp extends Application {
                     new AudioBrowserKoinModule().getModule(), // use this module only if you bought it
                     new ArCloudKoinModule().getModule(),
                     new TokenStorageKoinModule().getModule(),
+                    new VeUiSdkKoinModule().getModule(),
                     new VeFlowKoinModule().getModule(),
                     new IntegrationKoinModule().getModule(),
                     new GalleryKoinModule().getModule(),
                     new BanubaEffectPlayerKoinModule().getModule()
-            );
+            ).allowOverride(true);
             return null;
         });
     }
